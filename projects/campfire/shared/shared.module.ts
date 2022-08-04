735ee5b0427d37a1bdd73973d50110c3df5f4b66
AustinWildgrube <austin.wildgrube@ungerboeck.com>
2022-08-04 15:02:32 -0500

    refactor(shared): import single icons instead of the whole library

--- projects/campfire/shared/shared.module.ts
+++ projects/campfire/shared/shared.module.ts
@@ -2,8 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { fal } from '@fortawesome/pro-light-svg-icons';
-import { fas } from '@fortawesome/pro-solid-svg-icons';
+import { faChevronLeft, faChevronRight } from '@fortawesome/pro-regular-svg-icons';
+import { faCaretDown, faChevronLeft as fasChevronLeft, faChevronRight as fasChevronRight } from '@fortawesome/pro-solid-svg-icons';
+import {
+  faAngleDown,
+  faAngleUp,
+  faArrowDown,
+  faArrowUp,
+  faCalendarDay,
+  faCheckCircle,
+  faCoffee,
+  faExclamationCircle,
+  faExclamationTriangle,
+  faEye,
+  faEyeSlash,
+  faFilePdf,
+  faFileWord,
+  faFrown,
+  faInfoCircle,
+  faQuestionCircle,
+  faSortAlt,
+  faTimes,
+  faUser,
+} from '@fortawesome/pro-light-svg-icons';
 
 @NgModule({
   imports: [CommonModule, FontAwesomeModule],
@@ -12,6 +33,34 @@ import { fas } from '@fortawesome/pro-solid-svg-icons';
 })
 export class UsiSharedModule {
   constructor(library: FaIconLibrary) {
-    library.addIconPacks(fal, fas);
+    library.addIcons(
+      // Light & Regular
+      faAngleDown,
+      faAngleUp,
+      faArrowDown,
+      faArrowUp,
+      faCalendarDay,
+      faCaretDown,
+      faCheckCircle,
+      faChevronLeft,
+      faChevronRight,
+      faCoffee,
+      faExclamationCircle,
+      faExclamationTriangle,
+      faEye,
+      faEyeSlash,
+      faFilePdf,
+      faFileWord,
+      faFrown,
+      faInfoCircle,
+      faQuestionCircle,
+      faSortAlt,
+      faTimes,
+      faUser,
+
+      // Solid
+      fasChevronLeft,
+      fasChevronRight
+    );
   }
 }
