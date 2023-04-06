a878ec6aa60571c7697214ca3ff711d562117c3f
AustinWildgrube <austin.wildgrube@ungerboeck.com>
2023-04-06 10:36:15 -0500

    refactor(flex): remove unneeded import

--- projects/campfire/flex/flex.module.ts
+++ projects/campfire/flex/flex.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsiFlexComponent } from './flex.component';
-import { UsiUtilsModule } from 'usi-campfire/utils';
 
 @NgModule({
   declarations: [UsiFlexComponent],
   exports: [UsiFlexComponent],
-  imports: [CommonModule, UsiUtilsModule],
+  imports: [CommonModule],
 })
 export class UsiFlexModule {}
