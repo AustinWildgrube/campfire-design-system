fe4a65f547a8575dbd19749e4bf6f98f8585eb2e
AustinWildgrube <austin.wildgrube@ungerboeck.com>
2023-04-13 14:26:13 -0500

    test(multiselect): fix unit tests

--- projects/campfire/multiselect/tests/multiselect.component.spec.ts
+++ projects/campfire/multiselect/tests/multiselect.component.spec.ts
@@ -1,101 +1,99 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { Component, DebugElement } from '@angular/core';
-// import { By } from '@angular/platform-browser';
-//
-// import { UsiMultiselectComponent } from '../multiselect.component';
-// import { UsiMultiselectModule } from '../multiselect.module';
-// import { UsiSharedModule } from 'usi-campfire/shared';
-//
-// @Component({
-//   template: `
-//     <div style="width: 250px">
-//       <usi-multiselect
-//         [usiLabel]="usiLabel"
-//         [usiPlaceholder]="usiPlaceholder"
-//         [usiNoResultMessage]="usiNoResultMessage"
-//         [usiSearchable]="usiSearchable"
-//       ></usi-multiselect>
-//     </div>
-//   `,
-// })
-// class TestComponent extends UsiMultiselectComponent {}
-//
-// describe('MultiselectComponent', () => {
-//   let component: TestComponent;
-//   let fixture: ComponentFixture<TestComponent>;
-//   let debugElement: DebugElement;
-//
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [UsiSharedModule, UsiMultiselectModule],
-//       declarations: [UsiMultiselectComponent, TestComponent],
-//     }).compileComponents();
-//   });
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(TestComponent);
-//     component = fixture.componentInstance;
-//     debugElement = fixture.debugElement;
-//
-//     // component.usiData = [
-//     //   {
-//     //     label: 'Warsaw',
-//     //     value: 'wa',
-//     //     group: 'Europe',
-//     //   },
-//     //   {
-//     //     label: 'New York',
-//     //     value: 'ny',
-//     //     disabled: true,
-//     //   },
-//     //   {
-//     //     label: 'San Francisco',
-//     //     value: 'sf',
-//     //     group: 'America',
-//     //   },
-//     // ];
-//
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create a multiselect', () => {
-//     expect(component).toBeTruthy();
-//     expect(debugElement.nativeElement.querySelector('.usi-select')).toBeTruthy();
-//   });
-//
-//   it('should should have checkboxes in the dropdown', () => {
-//     const select = debugElement.query(By.css('.usi-input-group__input'));
-//     select.nativeElement.click();
-//     fixture.detectChanges();
-//
-//     expect(debugElement.nativeElement.querySelector('.usi-select__options')).toBeTruthy();
-//     expect(debugElement.nativeElement.querySelector('.usi-checkbox')).toBeTruthy();
-//   });
-//
-//   it('should allow multiple options to be selected', () => {
-//     const select = debugElement.query(By.css('.usi-input-group__input'));
-//     select.nativeElement.click();
-//     fixture.detectChanges();
-//
-//     const options = debugElement.queryAll(By.css('.usi-select__option'));
-//     options[0].nativeElement.click();
-//     options[1].nativeElement.click();
-//     fixture.detectChanges();
-//
-//     const badges = debugElement.nativeElement.querySelectorAll('.badge');
-//     expect(badges.length).toBe(2);
-//   });
-//
-//   it('should have a checked checkbox when selected', () => {
-//     const select = debugElement.query(By.css('.usi-input-group__input'));
-//     select.nativeElement.click();
-//     fixture.detectChanges();
-//
-//     const options = debugElement.queryAll(By.css('.usi-select__option'));
-//     options[0].nativeElement.click();
-//     fixture.detectChanges();
-//
-//     const checkbox = debugElement.nativeElement.querySelector('.usi-checkbox__input');
-//     expect(checkbox.classList).toContain('usi-checkbox__input--checked');
-//   });
-// });
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { Component, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+import { UsiMultiselectComponent } from '../multiselect.component';
+import { UsiMultiselectModule } from '../multiselect.module';
+import { UsiSharedModule } from 'usi-campfire/shared';
+import { UsiOptionComponent } from 'usi-campfire/select';
+import { FormGroupDirective } from '@angular/forms';
+
+@Component({
+  template: `
+    <div style="width: 250px">
+      <usi-multiselect [usiLabel]="usiLabel" [usiPlaceholder]="usiPlaceholder" [usiNoResultMessage]="usiNoResultMessage" [usiSearchable]="usiSearchable">
+        <usi-option [usiValue]="1">Option 1</usi-option>
+        <usi-option [usiValue]="2">Option 2</usi-option>
+        <usi-option [usiValue]="3" usiDisabled>Option 3</usi-option>
+      </usi-multiselect>
+    </div>
+  `,
+})
+class TestComponent extends UsiMultiselectComponent {}
+
+describe('MultiselectComponent', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let debugElement: DebugElement;
+  let select: DebugElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [UsiMultiselectComponent, UsiOptionComponent, TestComponent],
+      imports: [UsiSharedModule, UsiMultiselectModule],
+      providers: [FormGroupDirective],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+
+    select = debugElement.query(By.css('.usi-input-group__input'));
+    select.nativeElement.click();
+    fixture.detectChanges();
+
+    fixture.detectChanges();
+  });
+
+  it('should create a multiselect', () => {
+    expect(component).toBeTruthy();
+    expect(debugElement.query(By.css('.usi-select'))).toBeTruthy();
+  });
+
+  it('should should have checkboxes in the dropdown', () => {
+    expect(debugElement.nativeElement.querySelector('.usi-select__options')).toBeTruthy();
+    expect(debugElement.nativeElement.querySelector('.usi-checkbox')).toBeTruthy();
+  });
+
+  it('should allow multiple options to be selected', () => {
+    // option 1 is the controls
+    const options = debugElement.queryAll(By.css('.usi-select__option'));
+    options[1].nativeElement.click();
+    options[2].nativeElement.click();
+    fixture.detectChanges();
+
+    expect(select.nativeElement.value).toBe('2 Selected');
+  });
+
+  it('should only show selected options when the option is checked', () => {
+    // option 1 is the controls
+    const options = debugElement.queryAll(By.css('.usi-select__option'));
+    options[1].nativeElement.click();
+    fixture.detectChanges();
+
+    const showSelected = debugElement.query(By.css('.usi-checkbox__input'));
+    showSelected.nativeElement.click();
+    fixture.detectChanges();
+
+    const optionsCount = debugElement.queryAll(By.css('.usi-select__option')).length;
+    expect(optionsCount).toBe(2);
+  });
+
+  it('should clear the selected options when the clear button is clicked', () => {
+    // option 1 is the controls
+    const options = debugElement.queryAll(By.css('.usi-select__option'));
+    options[1].nativeElement.click();
+    options[2].nativeElement.click();
+    fixture.detectChanges();
+
+    expect(select.nativeElement.value).toBe('2 Selected');
+
+    const clear = debugElement.query(By.css('.usi-select__clear-all'));
+    clear.nativeElement.click();
+    fixture.detectChanges();
+
+    expect(select.nativeElement.value).toBe('');
+  });
+});
